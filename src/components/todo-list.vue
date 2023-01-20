ca43a9be974b34d21e334277e10a8cc88d499c20
Amir <amirrezwanoori@gmail.com>
2023-01-20 20:06:45 +0330

    first commit

---
+++ src/components/todo-list.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-list bordered separator>
+      <q-item clickable v-ripple>
+        <q-item-section>Single line item</q-item-section>
+      </q-item>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
